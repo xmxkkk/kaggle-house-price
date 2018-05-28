5e7847f5a975f27be7509b4e21f9a311b5e04cde
xmxkkk <908581016@qq.com>
2018-05-28 21:31:44 +0800

    add

--- input_data.py
+++ input_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm, skew
 from sklearn.preprocessing import LabelEncoder
 pd.set_option("display.max_columns",100)
 pd.set_option("display.max_rows",1000)
@@ -11,130 +12,81 @@ test_df=pd.read_csv("./data/test.csv")
 df=pd.read_csv("./data/train.csv")
 
 df=pd.concat([df,test_df])
-df.drop('SalePrice')
-df.drop('Id')
+df=df.drop(['SalePrice','Id'],axis=1)
 
 # print(df.columns)
 
 # print(df.corr()['LotFrontage'])
 
 df['MSSubClass']=df['MSSubClass'].apply(str)
-df['MSSubClass']=LabelEncoder().fit_transform(df['MSSubClass'])
-
-df['MSZoning']=df['MSZoning'].fillna(df['MSZoning'].mode()[0])
-df['MSZoning']=LabelEncoder().fit_transform(df['MSZoning'])
+df['GarageYrBlt']=df['GarageYrBlt'].apply(str)
 
 temp=df['LotArea'].copy(True)
 df['LotArea']=(df['LotArea']/1000).apply(int)
 df["LotFrontage"] = df.groupby("LotArea")["LotFrontage"].transform(lambda x: x.fillna(x.median()))
 df['LotArea']=temp
 
+df['Functional']=df['Functional'].fillna('Typ')
+df['OverallCond'] = df['OverallCond'].astype(str)
 
-df['Street']=LabelEncoder().fit_transform(df['Street'])
-df['LotShape']=LabelEncoder().fit_transform(df['LotShape'])
-df['LandContour']=LabelEncoder().fit_transform(df['LandContour'])
+col_fill_none=[
+    'MSSubClass','Street','Alley','LotShape','LandContour','LotConfig','LandSlope','Neighborhood',
+    'Condition1','Condition2','BldgType','HouseStyle','YearBuilt','YearRemodAdd','RoofStyle','RoofMatl',
+    'MasVnrType','ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',
+    'BsmtFinType2','Heating','HeatingQC','CentralAir','Functional','FireplaceQu',
+    'GarageType','GarageYrBlt','GarageFinish','GarageQual','GarageCond','PavedDrive','PoolQC','Fence','MiscFeature',
+    'YrSold','SaleCondition','OverallCond'
+]
 
-df['Utilities']=LabelEncoder().fit_transform(df['Utilities'].fillna(df['Utilities'].mode()[0]))
+col_fill_mode=[
+    'MSZoning','Utilities','Exterior1st','Exterior2nd','MasVnrArea','Electrical','KitchenQual','SaleType'
+]
 
-df['LotConfig']=LabelEncoder().fit_transform(df['LotConfig'])
+col_fill_0=[
+    'OverallQual','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','1stFlrSF','2ndFlrSF',
+    'LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr','KitchenAbvGr',
+    'TotRmsAbvGrd','Fireplaces','GarageCars','GarageArea','WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch',
+    'ScreenPorch','PoolArea','MiscVal','MoSold','LotFrontage'
+]
 
-df['LandSlope']=LabelEncoder().fit_transform(df['LandSlope'])
+for col in col_fill_none:
+    df[col]=LabelEncoder().fit_transform(df[col].fillna('None'))
 
-df['Neighborhood']=LabelEncoder().fit_transform(df['Neighborhood'])
+for col in col_fill_mode:
+    df[col] = LabelEncoder().fit_transform(df[col].fillna(df[col].mode()[0]))
 
-df['Condition1']=LabelEncoder().fit_transform(df['Condition1'])
-
-df['Condition2']=LabelEncoder().fit_transform(df['Condition2'])
-
-df['BldgType']=LabelEncoder().fit_transform(df['BldgType'])
-
-df['HouseStyle']=LabelEncoder().fit_transform(df['HouseStyle'])
-
-df['OverallQual']=LabelEncoder().fit_transform(df['OverallQual'])
-
-df['OverallCond']=LabelEncoder().fit_transform(df['OverallCond'])
-
-df['YearBuilt']=LabelEncoder().fit_transform(df['YearBuilt'])
-
-df['YearRemodAdd']=LabelEncoder().fit_transform(df['YearRemodAdd'])
-
-df['RoofStyle']=LabelEncoder().fit_transform(df['RoofStyle'])
-
-df['RoofMatl']=LabelEncoder().fit_transform(df['RoofMatl'])
-
-df['Exterior1st']=LabelEncoder().fit_transform(df['Exterior1st'].fillna(df['Exterior1st'].mode()[0]))
-
-df['Exterior2nd']=LabelEncoder().fit_transform(df['Exterior2nd'].fillna(df['Exterior2nd'].mode()[0]))
-
-df['MasVnrType']=LabelEncoder().fit_transform(df['MasVnrType'].fillna('None'))
-
-df['MasVnrArea']=df['MasVnrArea'].fillna(0)
-
-df['ExterQual']=LabelEncoder().fit_transform(df['ExterQual'])
-
-df['ExterCond']=LabelEncoder().fit_transform(df['ExterCond'])
-
-df['Foundation']=LabelEncoder().fit_transform(df['Foundation'])
-
-df['BsmtQual']=LabelEncoder().fit_transform(df['BsmtQual'].fillna('None'))
-
-df['BsmtCond']=LabelEncoder().fit_transform(df['BsmtCond'].fillna('None'))
-
-df['BsmtExposure']=LabelEncoder().fit_transform(df['BsmtExposure'].fillna('None'))
-
-df['BsmtFinType1']=LabelEncoder().fit_transform(df['BsmtFinType1'].fillna('None'))
-
-# df['BsmtFinSF1']=LabelEncoder().fit_transform(df['BsmtFinSF1'])
-
-df['BsmtFinSF1']=df['BsmtFinSF1'].fillna(0)
-
-df['BsmtFinSF2']=df['BsmtFinSF2'].fillna(0)
-
-df['BsmtUnfSF']=df['BsmtUnfSF'].fillna(0)
-
-df['TotalBsmtSF']=df['TotalBsmtSF'].fillna(0)
-df['1stFlrSF']=df['1stFlrSF'].fillna(0)
-df['2ndFlrSF']=df['2ndFlrSF'].fillna(0)
-
+for col in col_fill_0:
+    df[col] = df[col].fillna(0)
 
 df['TotalSF'] = df['TotalBsmtSF'] + df['1stFlrSF'] + df['2ndFlrSF']
-df.drop('TotalBsmtSF')
-df.drop('1stFlrSF')
-df.drop('2ndFlrSF')
+df=df.drop(['TotalBsmtSF','1stFlrSF','2ndFlrSF','Utilities'],axis=1)
 
-df['BsmtFinType2']=LabelEncoder().fit_transform(df['BsmtFinType2'].fillna('None'))
+# print(df.head(10))
 
-df['Heating']=LabelEncoder().fit_transform(df['Heating'])
-
-df['HeatingQC']=LabelEncoder().fit_transform(df['HeatingQC'])
-
-df['CentralAir']=LabelEncoder().fit_transform(df['CentralAir'])
-
-df['Electrical']=LabelEncoder().fit_transform(df['Electrical'].fillna('None'))
-# print(df['Electrical'])
-
-df['BsmtFullBath']=df['BsmtFullBath'].fillna(0)
-
-df['BsmtHalfBath']=df['BsmtHalfBath'].fillna(0)
-
-print(df['TotRmsAbvGrd'].isnull().any())
-
-df['KitchenQual']=LabelEncoder().fit_transform(df['KitchenQual'].fillna(df['KitchenQual'].mode()[0]))
-
-df['Functional']=LabelEncoder().fit_transform(df['Functional'].fillna('None'))
-print(df['Functional'])
+for x in df:
+    if df[x].isnull().any():
+        print(x)
 
 
-df['BsmtCond']=df['BsmtCond'].fillna(df['BsmtCond'].mode()[0])
-df['BsmtCond']=LabelEncoder().fit_transform(df['BsmtCond'])
+numeric_feats = df.dtypes[df.dtypes != "object"].index
 
-df['Alley']=LabelEncoder().fit_transform(df['Alley'].fillna('None'))
+        # Check the skew of all numerical features
+skewed_feats = df[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+print("\nSkew in numerical features: \n")
+skewness = pd.DataFrame({'Skew': skewed_feats})
+print(skewness.head(10))
 
+skewness = skewness[abs(skewness) > 0.75]
+print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
 
+from scipy.special import boxcox1p
+skewed_features = skewness.index
+lam = 0.15
+for feat in skewed_features:
+    #all_data[feat] += 1
+    df[feat] = boxcox1p(df[feat], lam)
 
-# for x in df:
-#     if df[x].isnull().any():
-#         print(x)
-
+df = pd.get_dummies(df)
+print(df.shape)
 # df['Neighborhood']=LabelEncoder().fit_transform(df['Neighborhood'])
 # print(df['LotFrontage'].corr(df['Neighborhood']))
