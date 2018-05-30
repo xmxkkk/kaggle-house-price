ca25984b0e1350174a88bb820d65ce055e87fc6b
xu.mingxun <xu.mingxun@puscene.com>
2018-05-30 10:15:28 +0800

    add

--- learn04.py
+++ learn04.py
@@ -4,12 +4,44 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import skew
 train=pd.read_csv('./data/train.csv')
-
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import make_pipeline
 
 df=pd.DataFrame({"x":[6,1,2,3],"y":[110,6,7,8]})
 
 print(df.apply(lambda x:skew(x)))
 
 print(train[['SalePrice']].apply(lambda x:skew(x)))
+
+
+model=make_pipeline(RobustScaler())
+
+print(df.values)
+result=RobustScaler().fit_transform(df.values)
+print(result)
+
+
+
+data=np.random.normal(0,10,1000)
+
+data=RobustScaler().fit_transform(data.reshape((-1,1)))
+
+plt.subplot(2,2,1)
+sns.distplot(data)
+
+plt.subplot(2,2,2)
+data=np.expm1(data)
+sns.distplot(data)
+
+plt.subplot(2,2,3)
+data=np.log1p(data)
+sns.distplot(data)
+
+# plt.subplot(2,2,4)
+# data=np.log1p(data)
+# sns.distplot(data)
+
+
+plt.show()
 
 
